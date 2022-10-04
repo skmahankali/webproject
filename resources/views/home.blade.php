e05f836a83fa5c070987746bea9f07a1241ef820
Sai Krishna Mahankali <saikrishna.mahankali98@gmail.com>
2022-10-04 11:57:25 -0400

    commit_1

---
+++ resources/views/home.blade.php
@@ -0,0 +1,136 @@
+@extends('layouts.app')
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+<style>
+    * {box-sizing: border-box;}
+
+body {
+  margin: 0;
+  font-family: Arial, Helvetica, sans-serif;
+}
+
+.topnav {
+  overflow: hidden;
+  background-color: #e9e9e9;
+}
+
+.topnav a {
+  float: left;
+  display: block;
+  color: black;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+  font-size: 17px;
+}
+
+.topnav a:hover {
+  background-color: #ddd;
+  color: black;
+}
+
+.topnav a.active {
+  background-color: #2196F3;
+  color: white;
+}
+
+.topnav .search-container {
+  float: right;
+}
+
+.topnav input[type=text] {
+  padding: 6px;
+  margin-top: 8px;
+  font-size: 17px;
+  border: none;
+}
+
+.topnav .search-container button {
+  float: right;
+  padding: 6px 10px;
+  margin-top: 8px;
+  margin-right: 16px;
+  background: #ddd;
+  font-size: 17px;
+  border: none;
+  cursor: pointer;
+}
+
+.topnav .search-container button:hover {
+  background: #ccc;
+}
+
+@media screen and (max-width: 600px) {
+  .topnav .search-container {
+    float: none;
+  }
+  .topnav a, .topnav input[type=text], .topnav .search-container button {
+    float: none;
+    display: block;
+    text-align: left;
+    width: 100%;
+    margin: 0;
+    padding: 14px;
+  }
+  .topnav input[type=text] {
+    border: 1px solid #ccc;  
+  }
+}
+</style>
+@section('content')
+<body>
+
+<div class="topnav">
+  <a class="active" href="#home">Home</a>
+  <a href="/profile/edit">Update details</a>
+  <a href="/profile/password">Update password</a>
+  <div class="search-container">
+    <form action="/action_page.php">
+      <input type="text" placeholder="Search.." name="search">
+      <button type="submit"><i class="fa fa-search"></i></button>
+    </form>
+  </div>
+</div>
+
+</body>
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Two Factor Authentication') }}</div>
+
+                <div class="card-body">
+                    @if (session('status') == "two-factor-authentication-disabled")
+                        <div class="alert alert-success" role="alert">
+                            Two factor Authentication has been disabled.
+                        </div>
+                    @endif
+
+                    @if (session('status') == "two-factor-authentication-enabled")
+                        <div class="alert alert-success" role="alert">
+                            Two factor Authentication has been enabled.
+                        </div>
+                    @endif
+
+
+                    <form method="POST" action="/user/two-factor-authentication">
+                        @csrf
+
+                        @if (auth()->user()->two_factor_secret)
+                            @method('DELETE')
+
+                            <div class="pb-5">
+                                {!! auth()->user()->twoFactorQrCodeSvg() !!}
+                            </div>
+
+                            <button class="btn btn-danger">Disable</button>
+                        @else
+                            <button class="btn btn-primary">Enable</button>
+                        @endif
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
